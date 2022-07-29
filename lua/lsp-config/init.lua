e3e8025a1135abf3cd2a96029060c9c35c1b13c1
Tarazonaa <andres.tara.so@gmail.com>
2022-07-28 19:57:17 -0500

    Started lsp-config properly

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -1,16 +1,32 @@
-require('lspconfig')['pyright'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags
-}
+-- C++
 require('lspconfig')['clangd'].setup{
-    on_attach = on_attach,
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+    end,
     flags = lsp_flags
 }
+-- Typescript
 require('lspconfig')['tsserver'].setup{
-    on_attach = on_attach,
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+    end,
     flags = lsp_flags
 }
-require('lspconfig')['rust_analyzer'].setup{
-    on_attach = on_attach,
+-- Javascript
+require('lspconfig')['eslint'].setup{
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+    end,
+    flags = lsp_flags
+}
+-- Rust
+require('lspconfig')['rls'].setup{
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+    end,
     flags = lsp_flags
 }
