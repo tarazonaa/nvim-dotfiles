4e1015011da3f08755d9a56558c05c7b7556ad40
Tarazonaa <andres.tara.so@gmail.com>
2022-07-28 20:36:12 -0500

    Done lsp things for now, onto cmp

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -5,7 +5,7 @@ snippet = {
   -- REQUIRED - you must specify a snippet engine
   expand = function(args)
     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
     -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
     -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
   end,
@@ -23,8 +23,8 @@ mapping = cmp.mapping.preset.insert({
 }),
 sources = cmp.config.sources({
   { name = 'nvim_lsp' },
-  { name = 'vsnip' }, -- For vsnip users.
-  -- { name = 'luasnip' }, -- For luasnip users.
+  -- { name = 'vsnip' }, -- For vsnip users.
+  { name = 'luasnip' }, -- For luasnip users.
   -- { name = 'ultisnips' }, -- For ultisnips users.
   -- { name = 'snippy' }, -- For snippy users.
 }, {
@@ -58,17 +58,3 @@ sources = cmp.config.sources({
   { name = 'cmdline' }
 })
 })
-
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['rust_analyzer'].setup {
-capabilities = capabilities
-}
-
-require('lspconfig')['eslint'].setup {
-capabilities = capabilities
-}
-require('lspconfig')['clangd'].setup {
-capabilities = capabilities
-}
