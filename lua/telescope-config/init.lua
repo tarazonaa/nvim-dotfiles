d064d5dc6e682f0f8626aec24b423ce80555e7cf
Tarazonaa <andres.tara.so@gmail.com>
2022-07-29 02:23:03 -0500

    Telescope commands started

--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -1,7 +1,11 @@
-require("telescope").setup({
+require("telescope").setup{
     defaults = {
-        mappings = {
-            ["<C-h>"] = "which_key"
-        }
+        prompt_prefix = "$ ",
+        selection_caret = "> ",
+        layout_strategy = 'center',
     }
-})
+}
+require('telescope').load_extension('fzf')
+vim.keymap.set('n', '<leader>en', '<cmd>Telescope find_files cwd=~/.config/nvim<Cr>', {})
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<Cr>', {})
+vim.keymap.set('n', '<leader>ft', '<cmd>Telescope git_files<Cr>', {})
